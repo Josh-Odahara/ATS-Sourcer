976e32f9c15d0c71d3b7629b19669f1d469770ad
Josh Odahara <josh.odahara@gmail.com>
2026-07-30 00:24:49 -0700

    small fixes

--- lib/sourcer/ats/greenhouse.ex
+++ lib/sourcer/ats/greenhouse.ex
@@ -5,23 +5,23 @@ defmodule Sourcer.ATS.Greenhouse do
     api_key = System.get_env("GREENHOUSE_API_KEY")
 
     case Req.get("https://harvest.greenhouse.io/v3/candidates",
-             headers: [accept: "application/json"],
-             auth: {:basic, {api_key, ""}}
-           ) do
-        {:ok, response} ->
-          Enum.map(response.body, fn candidate ->
-            %Sourcer.Candidate{
-              title: candidate["title"],
-              location: candidate["location"],
-              company: candidate["company"],
-              first_name: candidate["first_name"],
-              last_name: candidate["last_name"],
-              email: candidate["email"]
-            }
-          end)
+           headers: [accept: "application/json"],
+           auth: {:basic, {api_key, ""}}
+         ) do
+      {:ok, response} ->
+        Enum.map(response.body, fn candidate ->
+          %Sourcer.Candidate{
+            title: candidate["title"],
+            location: candidate["location"],
+            company: candidate["company"],
+            first_name: candidate["first_name"],
+            last_name: candidate["last_name"],
+            email: candidate["email"]
+          }
+        end)
 
-        {:error, reason} ->
-          {:error, reason}
-      end
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 end
